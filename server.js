7f6ed2bbb42e23610a6245c3db8e58d5da4944d7
Glitch (tan-aurora) <none>
2018-06-18 16:24:23 +0000

    👣😅 Checkpoint ./views/index.html:1289579/740 ./package.json:1289579/1280 ./server.js:1289579/1871

--- server.js
+++ server.js
@@ -4,9 +4,32 @@
 // init project
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var path = require('path');
+var fs = require('fs');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		let destination = path.join(process.cwd(), `/uploads/`);
+
+		if (!fs.existsSync(destination)) {
+			fs.mkdirSync(destination);
+		}
+		cb(null, destination);
+	},
+	filename: (req, file, cb) => {
+		let username = file.originalname;
+		let savedFileName = file.originalname;
+		req.body.file = file;
+		req.body.file.savedFileName = savedFileName;
+		req.body.file.username = username;
+		cb(null, savedFileName);
+	}
+});
+const upload = multer({ storage: storage });
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -15,6 +38,18 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.post("/api/upload", function(request, response){
+    upload.single('upfile')(request, response, (err) => {
+			if (err) {
+				console.log("Error while uploading file: ", err);
+				response.sendJson("There was some error while uploading file", 400);
+			}
+			else {
+				console.log(`File saved successfully for`);
+			}
+		});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
